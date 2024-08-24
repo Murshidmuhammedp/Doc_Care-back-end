cfc497daed7e9ca433b1b812785263fbb8ea05f0
Murshidmuhammedp <murshid.mmp@gmail.com>
2024-08-24 10:37:46 +0530

    cors update

--- server.js
+++ server.js
@@ -15,7 +15,8 @@ app.use(express.json());
 app.use(cors({
     // origin: "http://localhost:5173",
     origin: "https://doc-care-eight.vercel.app/",
-    credentials: true
+    methods:['GET','POST','PUT','PATCH','DELETE'],
+    allowedHeaders:['Content-Type','Authorization']
 }));
 
 app.use("/user/api", userRouter);
